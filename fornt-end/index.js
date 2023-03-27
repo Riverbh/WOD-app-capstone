b9ab72e551efd1d351c8dec4debac3330177e416
River Hansen <riverbh01@gmail.com>
2023-03-27 16:32:58 -0600

    created edited everything

--- fornt-end/index.js
+++ fornt-end/index.js
@@ -1,4 +1,26 @@
 const formA = document.getElementsByClassName('formA')
-const nameOne = document.getElementsByClassName('nameA')
+const nameA = document.getElementsByClassName('nameA')
+const date = document.getElementsByClassName('date')
+const typeWOD = document.getElementsByClassName('type-wod')
+const workoutA = document.getElementsByClassName('workoutA')
+const yesBtn = document.getElementsByClassName('yes')
+const noBtn = document.getElementsByClassName('no')
 
+
+formA.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    if (
+    nameA.value === '' ||
+    isNaN(+date.value) ||
+    isNaN(+typeWOD.value) ||
+    workoutA.value === '' ||
+    isNaN(+yesBtn.value) ||
+    isNaN(+noBtn.value)
+) {
+    alert('Some or Many inputs were not filled out correctly')
+    return
+}
+
+})
 
